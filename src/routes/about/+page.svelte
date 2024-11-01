d7f16ed14fdc1f492459565865a0ae99f7c9093c
ChrizxzFTW <55815776+Chrizxz@users.noreply.github.com>
2024-11-01 00:16:11 -0400

    Added a new template

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,6 +1,5 @@
-<h1>home</h1>
+<h1>about</h1>
 <nav>
   <a href="/">Home</a>
-  <a href="/about">About</a>
   <a href="/contact">Contact</a>
 </nav>
--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -1,6 +1,5 @@
-<h1>home</h1>
+<h1>contact</h1>
 <nav>
   <a href="/">Home</a>
   <a href="/about">About</a>
-  <a href="/contact">Contact</a>
 </nav>
