0e0067788dc18e3b0e42f96d9d917df7d0eae2b4
ChrizxzFTW <55815776+Chrizxz@users.noreply.github.com>
2024-11-02 19:20:43 -0400

    Additions and Tweaks
    - Added assets
    - Tweaked the layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,13 @@
 <script>
-	import { fade } from 'svelte/transition';
-
-	export let data;
+    import { fade } from 'svelte/transition';
+    export let data;
 </script>
 
-{#key data.pathname}
-	<div in:fade={{ duration: 200, delay: 600 }} out:fade={{ duration: 200 }}>
-		<slot />
-	</div>
-{/key}
+
+<main>
+    {#key data.pathname}
+        <div in:fade={{ duration: 200, delay: 400 }} out:fade={{ duration: 200 }}>
+            <slot />
+        </div>
+    {/key}
+</main>
