30427c8e678f49229de9edf1f9cca71b7cd7f16c
ChrizxzFTW <55815776+Chrizxz@users.noreply.github.com>
2024-11-18 13:47:38 -0500

    Tweaks and Optimizations
    - Tweaked the global colors file for organization
    - Optimized the site layout by turning certain features into components

---
+++ src/lib/components/BackToTop.svelte
@@ -0,0 +1,64 @@
+<script>
+    import '$css/colors.css';
+    import { onMount } from 'svelte';
+
+    onMount(() => {
+        const backToTopButton = document.getElementById("bttBtn");
+        // let lastScrollPosition = window.scrollY;
+
+        window.addEventListener('scroll', () => {
+            // when they scroll down 20px from the top
+            if (document.body.scrollTop > 120 || document.documentElement.scrollTop > 120) {
+                backToTopButton.style.display = "block";
+            } else {
+                backToTopButton.style.display = "none";
+            }
+        });
+
+    });
+
+    function backToTop() {
+        document.body.scrollTop = 0; // For Safari
+        document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+    }
+</script>
+
+<style>
+    #bttBtn {
+        display: none; 
+        position: fixed; 
+        bottom: 40px; 
+        right: 14.5px; 
+        z-index: 99;
+        color: var(--txt);
+        background-color: var(--glass);
+        backdrop-filter: blur(5px);
+        border: 1px solid var(--glassBord);
+        /* box-shadow: 0px 0px 10px rgba(0,0,0,0.3); */
+        cursor: pointer; 
+        border-radius: 40px; 
+        font-size: 28px; 
+        width: 70px; 
+        height: 70px;
+        text-align: center;
+        user-select: none;
+        transition-duration: 0.4s;
+    }
+    
+    #bttBtn:hover {
+        /* color: var(--txt); */
+        background-color: var(--glassHov); 
+    }
+    
+    .bttIcon {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+    }
+</style>
+
+<button on:click={backToTop} id="bttBtn" title="Back To Top" aria-label="Back To Top">
+    <div class="bttIcon">
+        <i class="fa-sharp fa-light fa-arrow-up"></i>
+    </div>
+</button>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,135 +1,16 @@
 <script>
     import { fade } from 'svelte/transition';
+	import BackToTopButton from '$lib/components/BackToTop.svelte';
+	import NavigationBar from '$lib/components/NavigationBar.svelte';
     export let data;
-	import '$css/navbar.css';
 	import '$css/global.css';
 	import '$lib/global.js';
-	import '$css/btt.css';
-	import { onMount } from 'svelte';
-
-	onMount(() => {
-		const backToTopButton = document.getElementById("bttBtn");
-		// let lastScrollPosition = window.scrollY;
-		const navbar = document.getElementById('navbar');
-		const revealElements = document.querySelectorAll('.reveal');
-
-		window.addEventListener('scroll', () => {
-			// when they scroll down 20px from the top
-			if (document.body.scrollTop > 120 || document.documentElement.scrollTop > 120) {
-				backToTopButton.style.display = "block";
-			} else {
-				backToTopButton.style.display = "none";
-			}
-			// scrolling down
-			if (window.scrollY > 50) {
-				navbar.classList.add('fixed');
-				// if (middleLogo) middleLogo.style.visibility = "visible";
-				return;
-			}
-			// scrolling up
-			navbar.classList.remove('fixed');
-			// if (middleLogo) middleLogo.style.visibility = "hidden";
-		});
-
-        const dropcheck = document.getElementById('dropcheck');
-        const dropdownContent = document.querySelector('.dropdownContent');
-        const fries = document.querySelector('.fries');
-
-        dropcheck.addEventListener('change', function() {
-            if (this.checked) {
-                dropdownContent.classList.add('slide-down');
-                dropdownContent.classList.remove('slide-up');
-                fries.classList.add('open');
-            } else {
-                dropdownContent.classList.add('slide-up');
-                dropdownContent.classList.remove('slide-down');
-                fries.classList.remove('open');
-            }
-        });
-
-		const label = document.getElementById('dropdownLabel');
-        const links = document.querySelectorAll('.dropdownContent a');
-        links.forEach(link => {
-            link.addEventListener('click', () => {
-                label.click();
-            });
-        });
-
-        document.addEventListener('click', (event) => {
-            if (!event.target.closest('.dropdown') && !event.target.closest('.dropbtn2') && dropcheck.checked) {
-                dropcheck.checked = false;
-                dropdownContent.classList.add('slide-up');
-                dropdownContent.classList.remove('slide-down');
-                fries.classList.remove('open');
-            }
-        });
-    });
-
-	function backToTop() {
-		document.body.scrollTop = 0; // For Safari
-		document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
-	}
 </script>
 
 <div class="container">
+	<NavigationBar/>
 
-	<!--Navbar-->
-	<nav id="navbar">
-		<!--mobile view-->
-		<div class="dropdown">
-			<input id="dropcheck" class="dropcheck" type="checkbox"/>
-			<h1>
-				<div class="dropbtnWrap">
-						<a href="/" title="Logo" class="mobileLogoWrap">
-							<img class="logoImg" src="imgs/navWording.svg" alt="Logo"/>
-						</a>
-					<label id="dropdownLabel" for="dropcheck" class="dropbtn2">
-						<!-- svelte-ignore a11y_missing_attribute -->
-						<a class="dropbtn clickit" title="Navigation Button" aria-label="Navigation Button">
-							<div class="fries">
-								<div class="line"></div>
-								<div class="line line2"></div>
-								<div class="line"></div>
-							</div>                              
-						</a>
-					</label>
-				</div>
-			</h1>
-			<div class="dropdownContent">
-				<a href="/about" title="About Me" aria-label="About Me">About Me</a>
-				<a href="/contact" title="Contact" aria-label="Contact">Contact</a>
-				<a href="https://beacons.ai/chrizxzftw" title="Other Links" rel="noopener noreferrer" target="_blank" aria-label="Other Links">Other Links</a>
-				<a class="icons" href="https://discord.gg/ZJ9PwQEC6E" title="Discord" rel="noopener noreferrer" target="_blank" aria-label="Discord"><i class="fab fa-discord"></i></a>
-				<a class="icons" href="https://twitter.com/ChrizxzFTW" title="Twitter" rel="noopener noreferrer" target="_blank" aria-label="Twitter"><i class="fab fa-x-twitter"></i></a>
-				<a class="icons" href="https://github.com/Chrizxz" title="Github" rel="noopener noreferrer" target="_blank" aria-label="Github"><i class="fab fa-github"></i></a>
-			</div>
-		</div>
-		<!--desktop view-->
-		<div class="desWrap">
-			<ul class="links">
-				<a class="desLogo" href="/" rel="noopener noreferrer">
-					<img src="imgs/navWording.svg" alt="Logo"/>
-				</a>
-				<div>
-					<li><a href="/about" title="About Me" aria-label="About Me">About Me</a></li>
-					<li><a href="/contact" title="Contact" aria-label="Contact">Contact</a></li>
-					<li><a href="https://beacons.ai/chrizxzftw" title="Other Links" rel="noopener noreferrer" target="_blank" aria-label="Other Links">Other Links</a></li>
-					<li><span class="separator" style="user-select: none;">|</span></li>
-					<li><a href="https://discord.gg/ZJ9PwQEC6E" title="Discord" rel="noopener noreferrer" target="_blank" aria-label="Discord"><i class="fab fa-discord"></i></a></li>
-					<li><a href="https://twitter.com/ChrizxzFTW" title="Twitter" rel="noopener noreferrer" target="_blank" aria-label="Twitter"><i class="fab fa-x-twitter"></i ></a></li>
-					<li><a href="https://github.com/Chrizxz" title="Github" rel="noopener noreferrer" target="_blank" aria-label="Github"><i class="fab fa-github"></i></a></li>
-				</div>
-			</ul>
-		</div>
-	</nav>
-
-	<!--Back to Top Button -->
-	<button on:click={backToTop} id="bttBtn" title="Back To Top" aria-label="Back To Top"> 
-		<div class="bttIcon">
-			<i class="fa-sharp fa-light fa-arrow-up"></i><br><br> 
-		</div>
-	</button>
-	<!-- Back to Top Button -->
+	<BackToTopButton/>
 
 	<main>
 		{#key data.pathname}
